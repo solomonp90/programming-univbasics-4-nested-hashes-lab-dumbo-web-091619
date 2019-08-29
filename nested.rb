b64dbb6db1b41c9904ff188b59a35c6e495c2f2e
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-29 18:49:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:grace_hopper][:known_for=>"COBOL"][:languages=>["COBOL","FORTRAN"]]= [:yukihiro_matsumoto][:known_for=>"Ruby"][:languages=>["LISP","C"]]
+programmer_hash[:yukihiro_matsumoto] = {:known_for=>"Ruby",:languages=>["LISP","C"]}
 end
 
 def changing_alan
