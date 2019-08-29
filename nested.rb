2d09435809eb398bdb6a31ab62ce9ae26adb1aab
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-29 19:37:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,6 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {:known_for=>"Ruby",:languages=>["LISP", "C"]}
 #programmer_hash[:alan_kay][:known_for] = "GUI"
 programmer_hash
-programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 #:known_for=>"Ruby",:languages=>["LISP", "C"]
